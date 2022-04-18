9e99c38a860d395df7d572d25e3fb2a6d7c4a273
Allen <74428917+Dragonwk@users.noreply.github.com>
2022-04-17 21:53:01 -0700

    Update options
    -Change the button text

---
+++ Assets/Dialogue.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue : MonoBehaviour
+{
+    public string name;
+    [TextArea(3,10)]
+    public string[] sentences;
+    public Dialogue[] next;
+    public string[] options;
+    
+    public Dialogue[] getNext()
+    {
+        return this.next;
+    }
+    public string[] getOptions()
+    {
+        return this.options;
+    }
+}
---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text DialogueText;
+    private Queue<string> sentences;
+    public DialogueTrigger[] options;
+    public Text[] optionNames;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting conversation with " + dialogue.name);
+        Debug.Log(nameText.text);
+        nameText.text = dialogue.name;
+        Debug.Log(dialogue.sentences);
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+        DialogueText.text = sentence;
+
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation.");
+    }
+    
+    public void OptionDialogue(Dialogue[] next, string[] option)
+    {
+        int i = 0;
+        foreach(DialogueTrigger d in options)
+        {
+            d.setDialogue(next[i]);
+            optionNames[i].text = option[i];
+            i++;
+        }
+    }
+}
