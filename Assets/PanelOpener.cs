21bc2b1cb452311a60a781e58f18a0885b2f16f9
Allen <allenchang55@yahoo.com>
2022-05-16 15:59:56 -0700

    Update to PanelOpener

--- Assets/PanelOpener.cs
+++ Assets/PanelOpener.cs
@@ -26,4 +26,45 @@ public class PanelOpener : MonoBehaviour
         }
     }
 
+    public void CloseO1()
+    {
+        O1.SetActive(false);
+    }
+    public void CloseO2()
+    {
+        O2.SetActive(false);
+    }
+    public void CloseO3()
+    {
+        O3.SetActive(false);
+    }
+    public void CloseO4()
+    {
+        O4.SetActive(false);
+    }
+    public void OpenAll()
+    {
+        if (DBox != null)
+        {
+            O1.SetActive(true);
+            O2.SetActive(true);
+            O3.SetActive(true);
+            O4.SetActive(true);
+            TButton.SetActive(true);
+            DBox.SetActive(true);
+        }
+    }
+    public void CloseAll()
+    {
+        if (DBox != null)
+        {
+            O1.SetActive(false);
+            O2.SetActive(false);
+            O3.SetActive(false);
+            O4.SetActive(false);
+            TButton.SetActive(false);
+            DBox.SetActive(false);
+        }
+    }
+
 }
